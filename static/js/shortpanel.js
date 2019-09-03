5183412d7ee85b1abd420055856aad9fd5cc9f8b
Peter Yang <me@peteryang.io>
2019-09-02 18:37:56 -0700

    refactor file directories

---
+++ static/js/shortpanel.js
@@ -0,0 +1,19 @@
+function popup(link) {
+    var host = location.protocol + "//" + location.hostname + "/";
+    prompt("Copy the selected link", host + link);
+}
+
+function edit(short){
+    document.getElementById("editField").value = short;
+    document.getElementById("editForm").submit();
+}
+
+function deleteItem(short) {
+    $.post(window.location.href, {"method" : "delete", "short" : short});
+    location.reload();
+}
+
+function cancel() {
+    // I don't know why location.reload() doesn't work
+    window.location = window.location.href;
+}
