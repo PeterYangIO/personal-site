ace43a2e3beca7b01a38a92359c5cd790408ee0f
Peter Yang <me@peteryang.io>
2019-09-07 02:07:57 -0700

    style / img cleanup

--- webapp/src/components/Page.tsx
+++ webapp/src/components/Page.tsx
@@ -3,19 +3,20 @@ import Header from "./Header";
 
 interface IPageProps {
     children?: JSX.Element | JSX.Element[];
+    className?: string;
     subtitle: string;
     title: string;
 }
 
 const Page: React.FC<IPageProps> = (props: IPageProps): JSX.Element => {
     return (
-        <>
+        <div className={props.className}>
             <Header
                 subtitle={props.subtitle}
                 title={props.title}
             />
             {props.children}
-        </>
+        </div>
     );
 };
 
--- webapp/src/pages/About.tsx
+++ webapp/src/pages/About.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Page from "../components/Page";
 import MainContent from "../components/MainContent";
+import logo from "../img/logo.svg";
+import "../styles/About.scss";
 
 const About: React.FC = (): JSX.Element => {
     return (
         <Page
+            className="about"
             subtitle="Hi. I'm an art driven techie and a tech driven artist."
             title="Peter Yang">
             <MainContent>
+                <div className="logo-container">
+                    <div className="shadow-container">
+                        <img
+                            alt="Peter logo"
+                            className="logo"
+                            src={logo}
+                        />
+                    </div>
+                </div>
                 <p>
                     I am a Senior studying Computer Science / Business Administration at the University of Southern California.
                     I recently completed my internship as a Software Engineer Intern at Microsoft and I'm currently looking for full time opportunities.
