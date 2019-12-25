394788e29a8a3a3721139b0f279a949e5f456403
Peter Yang <me@peteryang.io>
2019-12-25 01:56:32 -0800

    mobile responsive sidebar

--- webapp/src/Sidebar.tsx
+++ webapp/src/Sidebar.tsx
@@ -1,81 +1,104 @@
-import React from "react";
+import React, {useState} from "react";
 import NavigationItems from "./NavigationItems";
-import {faAddressBook, faCode, faFileAlt, faMusic, faPaintBrush, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {
+    faAddressBook,
+    faBars,
+    faCode,
+    faFileAlt,
+    faMusic,
+    faPaintBrush,
+    faTimes,
+    faUserCircle
+} from "@fortawesome/free-solid-svg-icons";
 import {faGithub, faLinkedin, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Sidebar: React.FC = (): JSX.Element => {
+    const [isOpen, setOpen] = useState(false);
+
     return (
-        <nav>
-            <div className="nav-links">
-                <div className="my-name">
-                    Peter Yang
+        <>
+            <div
+                id="nav-icon"
+                onClick={() => setOpen(!isOpen)}
+            >
+                <FontAwesomeIcon
+                    fixedWidth
+                    icon={isOpen ? faTimes : faBars}
+                />
+            </div>
+            <nav className={isOpen ? "" : "hide"}>
+                <div className="nav-links">
+                    <div className="my-name">
+                        Peter Yang
+                    </div>
+                    <NavigationItems
+                        header="Me"
+                        links={[
+                            {
+                                name: "About",
+                                href: "/",
+                                icon: faUserCircle
+                            },
+                            {
+                                name: "Contact",
+                                href: "/contact",
+                                icon: faAddressBook
+                            },
+                            {
+                                external: true,
+                                name: "Resume",
+                                href: "/docs/resume.pdf",
+                                icon: faFileAlt
+                            }
+                        ]}/>
+                    <NavigationItems
+                        header="Portfolio"
+                        links={[
+                            {
+                                name: "Software",
+                                href: "/software",
+                                icon: faCode
+                            },
+                            {
+                                name: "Orchestral music",
+                                href: "/music",
+                                icon: faMusic
+                            },
+                            {
+                                name: "Digital design",
+                                href: "/design",
+                                icon: faPaintBrush
+                            }
+                        ]}/>
+                    <NavigationItems
+                        header="Links"
+                        links={[
+                            {
+                                external: true,
+                                name: "GitHub",
+                                href: "https://github.com/peteryangio",
+                                icon: faGithub
+                            },
+                            {
+                                external: true,
+                                name: "LinkedIn",
+                                href: "https://www.linkedin.com/in/peteryangio",
+                                icon: faLinkedin
+                            },
+                            {
+                                external: true,
+                                name: "YouTube",
+                                href: "https://www.youtube.com/peteryang",
+                                icon: faYoutube
+                            }
+                        ]}/>
                 </div>
-                <NavigationItems
-                    header="Me"
-                    links={[
-                        {
-                            name: "About",
-                            href: "/",
-                            icon: faUserCircle
-                        },
-                        {
-                            name: "Contact",
-                            href: "/contact",
-                            icon: faAddressBook
-                        },
-                        {
-                            external: true,
-                            name: "Resume",
-                            href: "/docs/resume.pdf",
-                            icon: faFileAlt
-                        }
-                    ]}/>
-                <NavigationItems
-                    header="Portfolio"
-                    links={[
-                        {
-                            name: "Software",
-                            href: "/software",
-                            icon: faCode
-                        },
-                        {
-                            name: "Orchestral music",
-                            href: "/music",
-                            icon: faMusic
-                        },
-                        {
-                            name: "Digital design",
-                            href: "/design",
-                            icon: faPaintBrush
-                        }
-                    ]}/>
-                <NavigationItems
-                    header="Links"
-                    links={[
-                        {
-                            external: true,
-                            name: "GitHub",
-                            href: "https://github.com/peteryangio",
-                            icon: faGithub
-                        },
-                        {
-                            external: true,
-                            name: "LinkedIn",
-                            href: "https://www.linkedin.com/in/peteryangio",
-                            icon: faLinkedin
-                        },
-                        {
-                            external: true,
-                            name: "YouTube",
-                            href: "https://www.youtube.com/peteryang",
-                            icon: faYoutube
-                        }
-                    ]}/>
-            </div>
-            <div className="font-light">
-                &copy; 2016 &ndash; {new Date().getFullYear()}
-            </div>
-        </nav>
+                <div className="font-light">
+                    &copy; 2016 &ndash; {new Date().getFullYear()}
+                </div>
+            </nav>
+        </>
     );
 };
 
