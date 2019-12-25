fd81f650e0358ace3e5a78238f0759b980338474
Peter Yang <me@peteryang.io>
2019-12-25 02:25:49 -0800

    added more content to redirect page

--- webapp/src/pages/Redirect.tsx
+++ webapp/src/pages/Redirect.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import MainContent from "../components/MainContent";
+import Page from "../components/Page";
 
 class Redirect extends React.Component {
     constructor(props: {}) {
         super(props);
-        window.location.href = `/short.php?val=${window.location.pathname}`;
+        window.location.href = Redirect._redirectLink;
     }
 
-    public render(): null {
-        return null;
+    public render(): JSX.Element {
+        return (
+            <Page subtitle="Taking you to another page" title="Redirecting">
+                <MainContent>
+                    <div>
+                        If you are not redirected within 5 seconds, click <a href={Redirect._redirectLink}>here</a>.
+                    </div>
+                </MainContent>
+            </Page>
+        );
+    }
+
+    private static get _redirectLink(): string {
+        return `/short.php?val=${window.location.pathname}`;
     }
 }
 
