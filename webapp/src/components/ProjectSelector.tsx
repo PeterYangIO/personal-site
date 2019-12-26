641f8d683e6fc0da321cefce2829f0452372db42
Peter Yang <me@peteryang.io>
2019-12-25 16:37:17 -0800

    switch drag scroll provider

--- webapp/src/components/ProjectSelector.tsx
+++ webapp/src/components/ProjectSelector.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {MouseEventHandler} from "react";
 import "../styles/ProjectSelector.scss";
 import ProjectTile from "./ProjectTile";
 import {IProject} from "../models/IProject";
-import ScrollContainer from "react-indiana-drag-scroll";
+import DragScrollProvider from "drag-scroll-provider";
 
 interface IProjectSelectorProps {
     projects: IProject[];
@@ -10,25 +10,39 @@ interface IProjectSelectorProps {
     setProject: (project: IProject) => void;
 }
 
+interface IDragScrollProviderChildren {
+    onMouseDown: MouseEventHandler<any>;
+    ref: React.RefObject<any>;
+    clickItem: (onClick: VoidFunction) => void;
+}
+
 const ProjectSelector: React.FC<IProjectSelectorProps> = (props: IProjectSelectorProps): JSX.Element => {
     return (
-        <ScrollContainer className="scroll-container">
-            <div className="project-selector">
-                {
-                    props.projects.map((item: IProject, index: number): JSX.Element => {
-                        return (
-                            <ProjectTile
-                                isActive={item === props.selectedProject}
-                                key={index}
-                                onClick={(): void => props.setProject(item)}
-                                img={item.img}
-                                title={item.title}
-                            />
-                        );
-                    })
-                }
-            </div>
-        </ScrollContainer>
+        <DragScrollProvider>
+            {
+                ({ onMouseDown, ref, clickItem }: IDragScrollProviderChildren) => (
+                    <div
+                        className="project-selector"
+                        ref={ref}
+                        onMouseDown={onMouseDown}
+                    >
+                        {
+                            props.projects.map((item: IProject, index: number): JSX.Element => {
+                                return (
+                                    <ProjectTile
+                                        isActive={item === props.selectedProject}
+                                        key={index}
+                                        onClick={(): void => clickItem(() => props.setProject(item))}
+                                        img={item.img}
+                                        title={item.title}
+                                    />
+                                );
+                            })
+                        }
+                    </div>
+                )
+            }
+        </DragScrollProvider>
     );
 };
 
---
+++ webapp/src/types/drag-scroll-provider.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react">
+declare module "drag-scroll-provider" {
+    export default class DragScrollProvider extends React.Component {}
+}
